69432d83c25a3fb522436c518f26095c9324b2d3
Mikhail Dudkin from home pc <dudkinmn@yandex.ru>
2019-09-25 00:07:09 +0300

    second lesson 1/2 part

--- main.js
+++ main.js
@@ -15,8 +15,7 @@ const keys = {
 const setting = {
     start: false,
     score: 0,
-    speed: 3
-
+    speed: 3,
 };
 
 start.addEventListener('click', startGame);
@@ -27,15 +26,47 @@ document.addEventListener('keyup', stopRun);
 
 function startGame() {
     start.classList.add('hide');
+
+    for (let i = 0; i < 20; i++) {
+        const line = document.createElement('div');
+        line.classList.add('line');
+        line.style.top = (i * 75) +'px';
+        line.y = i * 100;
+        gameArea.appendChild(line);
+        
+    }
+
     setting.start = true;
+    
     gameArea.appendChild(car);
+    setting.x = car.offsetLeft;
+    setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
+
 
 }
 
 function playGame() {
     console.log('Play game!');
+
+    moveRoad();
+
     if (setting.start) {
+        if (keys.ArrowLeft && setting.x > 0) {
+            setting.x -= setting.speed;    
+        }
+        if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) {
+            setting.x += setting.speed;
+        }
+        if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)  ) {
+            setting.y += setting.speed;
+        }
+        if (keys.ArrowUp && setting.y > 0) {
+            setting.y -= setting.speed;            
+        }
+
+        car.style.left = setting.x + 'px';
+        car.style.top = setting.y + 'px';
         requestAnimationFrame(playGame);
     }
     
@@ -49,6 +80,22 @@ function startRun(event) {
 function stopRun() {
     event.preventDefault();
     keys[event.key] = false;
+}
+
+function moveRoad() {
+    
+    let lines = document.querySelectorAll('.line');
+    lines.forEach((line) => {
+        console.log('move line');
+        line.y += setting.speed;
+        line.style.top = line.y + 'px';
+
+        
+        if (line.y >= document.documentElement.clientHeight) {
+            
+        }
+    });
+    
 }
 
 
